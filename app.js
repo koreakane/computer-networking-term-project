2494b539b9d490043b2a1acc30a8f6a7823dc6b2
손창범 <nuclearwinte@gmail.com>
2021-06-22 19:45:56 +0900

    backend init

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 2,
+    },
+  })
+);
 
 const server = app.listen(4000, function () {
   console.log("Express server has started on port 4000");
 });
 
-const key = 1;
-const value = "goodbye";
-
 app.get("/login", function (req, res) {
-  res.cookie(key, value, {});
   res.redirect("/");
 });
 
@@ -20,6 +36,30 @@ app.get("/state", function (req, res) {
   res.send("cookie : " + req.cookies.key);
 });
 
-app.get("/", (req, res) => {
-  res.send("LOL\n");
+let exampleInfo = {
+  id: "101",
+  name: "손창범",
+  authorized: true,
+  number: 1,
+};
+
+app.get("/", function (req, res) {
+  console.log(req.session);
+
+  if (!req.session.info) {
+    req.session.info = exampleInfo;
+  } else {
+    req.session.info = {
+      ...exampleInfo,
+      number: req.session.info.number + 1,
+    };
+  }
+
+  res.cookie("user", req.session.info, {
+    maxAge: 9000000,
+    sameSite: true,
+  });
+  res.send(req.session.info);
+
+  // console.log(req.cookies);
 });
