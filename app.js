78c7e27bd2dd1f19f2589c2a02353c7b129b2df3
손창범 <nuclearwinte@gmail.com>
2021-06-23 23:08:49 +0900

    gg

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(
   cors({
@@ -24,6 +26,14 @@ app.use(
   })
 );
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
 const server = app.listen(4000, function () {
   console.log("Express server has started on port 4000");
 });
@@ -36,30 +46,80 @@ app.get("/state", function (req, res) {
   res.send("cookie : " + req.cookies.key);
 });
 
-let exampleInfo = {
-  id: "101",
-  name: "손창범",
+let exampleInfo_cookie = {
+  id: 99,
+  name: "쿠키",
   authorized: true,
-  number: 1,
+  counter: 1,
+};
+
+let exampleInfo_jwt = {
+  id: 100,
+  name: "jwt",
+  authorized: true,
+  counter: 1,
 };
 
 app.get("/", function (req, res) {
   console.log(req.session);
 
-  if (!req.session.info) {
-    req.session.info = exampleInfo;
-  } else {
-    req.session.info = {
-      ...exampleInfo,
-      number: req.session.info.number + 1,
-    };
+  res.send(req.session.info_cookie);
+});
+
+app.get("/get-cookie", function (req, res) {
+  console.log(req.session);
+
+  if (!req.session.info_cookie) {
+    req.session.info_cookie = exampleInfo_cookie;
   }
 
-  res.cookie("user", req.session.info, {
+  res.cookie("user", req.session.info_cookie, {
     maxAge: 9000000,
     sameSite: true,
   });
-  res.send(req.session.info);
+  res.send(req.session.info_cookie);
 
   // console.log(req.cookies);
+});
+
+app.post("/post-cookie", function (req, res) {
+  console.log(req.body);
+
+  req.session.info_cookie = req.body.info_cookie;
+
+  res.send(req.session.info_cookie);
+});
+
+const generateAccessToken = (data) => {
+  return jwt.sign(data, "SECRET", {
+    expiresIn: "15m",
+  });
+};
+
+app.get("/get-jwt", function (req, res) {
+  console.log(req.session);
+
+  if (!req.session.info_jwt) {
+    req.session.info_jwt = exampleInfo_jwt;
+  }
+
+  res.json({
+    info_jwt: req.session.info_jwt,
+    accessToken: generateAccessToken(req.session.info_jwt),
+  });
+});
+
+app.post("/post-jwt", function (req, res) {
+  console.log(req.body);
+
+  // const clientToken = req.body.token;
+
+  // jwt.verify(clientToken, "SECRET")
+
+  req.session.info_jwt = req.body.info_jwt;
+
+  res.json({
+    info_jwt: req.session.info_jwt,
+    accessToken: generateAccessToken(req.session.info_jwt),
+  });
 });
